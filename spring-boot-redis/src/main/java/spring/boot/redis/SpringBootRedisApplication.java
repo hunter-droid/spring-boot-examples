ff0d56ddcaf5cbd425bd27b8bc16af584b1e7a34
fx.yu <458925392@qq.com>
2019-09-26 19:11:53 +0800

    add spring-boot-redis

---
+++ spring-boot-redis/src/main/java/spring/boot/redis/SpringBootRedisApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisApplication.class, args);
+    }
+
+}
